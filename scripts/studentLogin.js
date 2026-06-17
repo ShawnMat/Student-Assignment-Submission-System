9ae010514086ae4e85e20ba84c38381151a432cd
ShawnMat <shawn.daniel1919@gmail.com>
2026-06-17 17:33:26 +0530

    Doing changes on trainer dashboard

--- scripts/studentLogin.js
+++ scripts/studentLogin.js
@@ -1,13 +1,33 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const submitBtn=document.querySelector('.submit')
-    const username = document.querySelector('#username')
-    const password = document.querySelector('#password')
+const API = 'http://localhost:3000'
 
+    $('.submit').click(async function (username,password) {
+        try{
+            const username = document.getElementById('username').value
+            const password = document.getElementById('password').value 
+            const response = await fetch(`${API}/students`)
+            const users = await response.json()
+            const values = users.find(users=>{   
+                // console.log(username);
+                // console.log(password);
+                // console.log(users.Username);
+                // console.log(users.Password);
+                           
+                if(users.Username === username && users.Password === password){
+                    console.log("Success!!");
+                    window.location.replace("/pages/studentSampleDashboard.html")
+                }
+                else{
+                    console.log("Sorry, Couldn't connect you to server.");
+                    
+                }
+            })
+            // console.log(values);
+            
+        }
+        catch{
+            console.log("Error");
+            
+        }
 
-
-
-
-    submitBtn.addEventListener('click',()=>{
-        window.location.href = "/pages/studentDashboard.html"
-    })
+    
 })
---
+++ scripts/trainerDashboard.js
@@ -0,0 +1,12 @@
+// $('.workArea').hide()
+// $('.workArea').show()
+// $('.classArea').hide()
+// $('.classArea').show()
+$('#createAssignmentBtn').click(()=>{
+    const CREATE_ASSIGNMENT_MODAL = bootstrap.Modal.getInstance(
+        document.getElementById('createAssignmentModal')
+    )
+    CREATE_ASSIGNMENT_MODAL.hide()
+
+})
+
